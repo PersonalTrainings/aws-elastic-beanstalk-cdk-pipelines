18e856a70193f7fee09b61569f454d005d4e6d13
Narek Khachatryan <Narek_Khachatryan1@epam.com>
2023-10-02 12:08:20 +0400

    Update account number, region and repo configs

--- lib/cdk-pipeline-stack.ts
+++ lib/cdk-pipeline-stack.ts
@@ -1,8 +1,8 @@
-import { CodePipeline, CodePipelineSource, ShellStep } from 'aws-cdk-lib/pipelines';
-import { Construct } from 'constructs';
-import {  Stack, StackProps } from 'aws-cdk-lib';
+import { CodePipeline, CodePipelineSource, ShellStep } from "aws-cdk-lib/pipelines";
+import { Construct } from "constructs";
+import { Stack, StackProps } from "aws-cdk-lib";
 
-import { CdkEBStage } from './eb-stage';
+import { CdkEBStage } from "./eb-stage";
 
 /**
  * The stack that defines the application pipeline
@@ -11,35 +11,30 @@ export class CdkPipelineStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    const pipeline = new CodePipeline(this, 'Pipeline', {
+    const pipeline = new CodePipeline(this, "Pipeline", {
       // The pipeline name
-      pipelineName: 'MyServicePipeline',
+      pipelineName: "MyServicePipeline",
 
-       // How it will be built and synthesized
-       synth: new ShellStep('Synth', {
-         // Where the source can be found
-         input: CodePipelineSource.gitHub('OWNER/REPO', 'main'),
-         
-         // Install dependencies, build and run cdk synth
-         installCommands: ['npm i -g npm@latest'],
-         commands: [
-           'npm ci',
-           'npm run build',
-           'npx cdk synth'
-         ],
-       }),
+      // How it will be built and synthesized
+      synth: new ShellStep("Synth", {
+        // Where the source can be found
+        input: CodePipelineSource.gitHub("PersonalTrainings/aws-elastic-beanstalk-cdk-pipelines", "main"),
+
+        // Install dependencies, build and run cdk synth
+        installCommands: ["npm i -g npm@latest"],
+        commands: ["npm ci", "npm run build", "npx cdk synth"],
+      }),
     });
 
     // This is where we add the application stages
-    // For environment with default values 
+    // For environment with default values
     // const deploy = new CdkEBStage(this, 'Pre-Prod');
 
     // For environment with custom AutoScaling group configuration
-    const deploy = new CdkEBStage(this, 'Pre-Prod', { 
-      minSize : "1",
-      maxSize : "1"
-  });
-    const deployStage = pipeline.addStage(deploy); 
-    
+    const deploy = new CdkEBStage(this, "Pre-Prod", {
+      minSize: "1",
+      maxSize: "1",
+    });
+    const deployStage = pipeline.addStage(deploy);
   }
 }
