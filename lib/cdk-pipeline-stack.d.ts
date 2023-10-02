49e1309d534eb15da572f2b8e0579613c0cf0ddb
Narek Khachatryan <Narek_Khachatryan1@epam.com>
2023-10-02 13:19:10 +0400

    Update gitignore, aws-cdk-lib version

--- lib/cdk-pipeline-stack.d.ts
+++
@@ -1,8 +0,0 @@
-import { Construct } from "constructs";
-import { Stack, StackProps } from "aws-cdk-lib";
-/**
- * The stack that defines the application pipeline
- */
-export declare class CdkPipelineStack extends Stack {
-    constructor(scope: Construct, id: string, props?: StackProps);
-}
--- lib/eb-stage.d.ts
+++
@@ -1,9 +0,0 @@
-import { Stage } from 'aws-cdk-lib';
-import { Construct } from 'constructs';
-import { EBEnvProps } from './eb-appln-stack';
-/**
- * Deployable unit of web service app
- */
-export declare class CdkEBStage extends Stage {
-    constructor(scope: Construct, id: string, props?: EBEnvProps);
-}
